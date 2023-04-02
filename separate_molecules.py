0e93ed7cb90f2050f18b00653bd597f290209418
Diogo Mealha <113041969+diogoMealha@users.noreply.github.com>
2023-04-02 23:13:21 +0100

    Update separate_molecules.py

--- separate_molecules.py
+++ separate_molecules.py
@@ -1,25 +1,32 @@
-mesh_info = {}
-file_path__ = None
-with open('molecules_mesh.txt', 'r') as f:
-    for line in f.readlines():
-        if(line[0] == 'M' or line == "end"):
-            if file_path__ != None:
-                mesh_info[file_path__][0] = str(len(mesh_info[file_path__]) - 1) + "\n"
-             
-            file_path__ = line[:-1].replace(" ", "_")
-            if(line != "end"):
-                mesh_info[file_path__] = [0]
-        else:
-            mesh_info[file_path__].append(line)
+def main():
+    PATH = "FOLDER/"
+    COMMENT = "Carbon bond length: "
+    MESH_FILE = "molecules_mesh.txt"
 
-PATH = "FOLDER/"
-COMMENT = "Carbon bond length: "
-for key in mesh_info.keys():
-    with open(PATH + key + ".xyz", 'w') as f:
-        f.write(mesh_info[key][0])
-        f.write(COMMENT + "\n")
-        for line in mesh_info[key][1:]:
-            foo = line.split()
-            f.write(f'{foo[0]} \t {foo[1]} \t {foo[2]} \t {foo[3]}\n')
+    mesh_info_dic = {}
+    file_name = None
+    #store data
+    with open(MESH_FILE, 'r') as f:
+        for line in f.readlines():
+            if(line[0] == 'M' or line == "end"):
+                if file_name is not None:
+                    mesh_info_dic[file_name][0] = str(len(mesh_info_dic[file_name]) - 1) + "\n"
+                file_name = line[:-1].replace(" ", "_")
+                if(line != "end"):
+                    mesh_info_dic[file_name] = [0]
+            else:
+                mesh_info_dic[file_name].append(line)
+        f.close()
+
+    #write data
+    for key in mesh_info_dic.keys():
+        with open(PATH + key + ".xyz", 'w') as f:
+            f.write(mesh_info_dic[key][0])
+            f.write(COMMENT + "\n")
+            for line in mesh_info_dic[key][1:]:
+                foo = line.split()
+                f.write(f'{foo[0]} \t {foo[1]} \t {foo[2]} \t {foo[3]}\n')
+
+if __name__ == "__main__": main()
 
 
