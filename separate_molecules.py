65cd8b9080cd0d0a02bddc722b596e19af46d53c
Diogo Mealha <113041969+diogoMealha@users.noreply.github.com>
2023-05-28 18:37:03 +0100

    Update separate_molecules.py

--- separate_molecules.py
+++ separate_molecules.py
@@ -1,14 +1,14 @@
-def main():
-    PATH = "FOLDER/"
-    COMMENT = "Carbon bond length: "
-    MESH_FILE = "molecules_mesh.txt"
+def sep_mol(dest_folder, mesh_file_name):
+    PATH = dest_folder + "/"
+    COMMENT = "Comments: "
+    MESH_FILE = mesh_file_name
 
     mesh_info_dic = {}
     file_name = None
-    #store data
+    # store data
     with open(MESH_FILE, 'r') as f:
         for line in f.readlines():
-            if(line[0] == 'M' or line == "end"):
+            if line[0] == 'M' or line == "end":
                 if file_name is not None:
                     mesh_info_dic[file_name][0] = str(len(mesh_info_dic[file_name]) - 1) + "\n"
                 file_name = line[:-1].replace(" ", "_")
@@ -18,15 +18,14 @@ def main():
                 mesh_info_dic[file_name].append(line)
         f.close()
 
-    #write data
+    # write data
     for key in mesh_info_dic.keys():
         with open(PATH + key + ".xyz", 'w') as f:
             f.write(mesh_info_dic[key][0])
-            f.write(COMMENT + "\n")
+            f.write("Name: " + key + " " + COMMENT + "\n")
             for line in mesh_info_dic[key][1:]:
                 foo = line.split()
                 f.write(f'{foo[0]} \t {foo[1]} \t {foo[2]} \t {foo[3]}\n')
 
-if __name__ == "__main__": main()
-
-
+    # return list of all molecule names
+    return [mol_name for mol_name in mesh_info_dic.keys()]
